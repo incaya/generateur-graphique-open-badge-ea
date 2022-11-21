5ff4efa3bb9beeb162d69b797e45674904c67cd5
Thomas Maziere <thomas.maziere@incaya.fr>
2022-11-21 11:37:46 +0100

    feat: affichage des badges courants pour EA

--- src/Recurrents.js
+++ src/Recurrents.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Pc2, Xp3, Sv2, Sf2, Cp3, Gp3, Pc3 } from "./Badges";
+import { Cp2, Gp2, Pj3, Sv2, Sf2, Cp3, Gp3, Pc3, Pc1 } from "./Badges";
 
 function Recurrents() {
 
@@ -12,75 +12,43 @@ function Recurrents() {
       </p>
       <div className="blocks-row">
         <div className="recurrent-choice">
-          <a href={process.env.PUBLIC_URL + "/customize/index.html?id=pc2"}>
-            <p>
-              Reconnaître l'entrée dans un parcours de formation de découverte,
-              d'apprentissage
-            </p>
-            <Pc2 />
-          </a>
-        </div>
-        <div className="recurrent-choice">
-          <a href={process.env.PUBLIC_URL + "/customize/index.html?id=xp3"}>
-            <p>
-              Reconnaitre la participation à une session de formation, une
-              expérience, un événement
-            </p>
-            <Xp3 />
-          </a>
-        </div>
-        <div className="recurrent-choice">
           <a href={process.env.PUBLIC_URL + "/customize/index.html?id=sv2"}>
-            <p>Reconnaitre une initiation, un test d'un savoir</p>
+            <p>
+            Reconnaitre l'aquisition d'un savoir
+            </p>
             <Sv2 />
-          </a>
-        </div>
-        <div className="recurrent-choice">
-          <a href={process.env.PUBLIC_URL + "/customize/index.html?id=sv2"}>
-            <p>Reconnaître la capacité à transmettre un savoir</p>
-            <Sv2 />
-          </a>
-        </div>
-        <div className="recurrent-choice">
-          <a href={process.env.PUBLIC_URL + "/customize/index.html?id=sf2"}>
-            <p>Reconnaître une initiation, un test, un savoir faire</p>
-            <Sf2 />
-          </a>
-        </div>
-        <div className="recurrent-choice">
-          <a href={process.env.PUBLIC_URL + "/customize/index.html?id=sf2"}>
-            <p>Reconnaître la capacité à transmettre un savoir faire</p>
-            <Sf2 />
-          </a>
-        </div>
-        <div className="recurrent-choice">
-          <a href={process.env.PUBLIC_URL + "/customize/index.html?id=gp3"}>
-            <p>Reconnaître l'engagement actif dans une communauté apprenante</p>
-            <Gp3 />
-          </a>
-        </div>
-        <div className="recurrent-choice">
-          <a href={process.env.PUBLIC_URL + "/customize/index.html?id=cp3"}>
-            <p>Reconnaître la pratique amateure</p>
-            <Cp3 />
           </a>
         </div>
         <div className="recurrent-choice">
           <a href={process.env.PUBLIC_URL + "/customize/index.html?id=cp3"}>
             <p>
-              Reconnaître la capacité à transmettre une pratique associant
-              savoir, savoir faire et savoir être
+            Reconnaitre une pratique, une compétence
             </p>
-            <Cp3 />
+            <Cp2 />
           </a>
         </div>
         <div className="recurrent-choice">
-          <a href={process.env.PUBLIC_URL + "/customize/index.html?id=pc3"}>
-            <p>
-              Se créer un badge de rêve ou reconnaître l'aboutissement d'un
-              parcours individuel
-            </p>
-            <Pc3 />
+          <a href={process.env.PUBLIC_URL + "/customize/index.html?id=sf2"}>
+            <p>Reconnaitre un savoir faire</p>
+            <Sf2 />
+          </a>
+        </div>
+        <div className="recurrent-choice">
+          <a href={process.env.PUBLIC_URL + "/customize/index.html?id=gp2"}>
+            <p>Reconnaitre sa contribution dans un groupe</p>
+            <Gp2 />
+          </a>
+        </div>
+        <div className="recurrent-choice">
+          <a href={process.env.PUBLIC_URL + "/customize/index.html?id=pj3"}>
+            <p>Reconnaitre le pilotage ou l'animation d'un projet</p>
+            <Pj3 />
+          </a>
+        </div>
+        <div className="recurrent-choice">
+          <a href={process.env.PUBLIC_URL + "/customize/index.html?id=pc1"}>
+            <p>Reconnaitre l'engagement dans un parcours</p>
+            <Pc1 />
           </a>
         </div>
       </div>
